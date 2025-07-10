a0c7060188287ac4cd72bc667bdb663f70765991
Jacob Mazelin <jjmazelin@gmail.com>
2025-07-10 17:38:49 -0400

    Commit

--- lidar_bot/lidar_bot/subscriber.py
+++ lidar_bot/lidar_bot/subscriber.py
@@ -27,7 +27,7 @@ def main(args=None):
     rclpy.spin(minimal_subscriber)
 
     # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
+    # (optional - otherwise it will be done automatically 
     # when the garbage collector destroys the node object)
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
